c377f71367b6e321715b0ab78d8b6cdc78757648
Axel Sandstedt <axel.sandstedt97@protonmail.com>
2025-12-05 15:42:36 +0100

    Base for deb installer

---
+++ install.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+if !(command -v cmake > /dev/null 2>&1); then
+	echo "Error: CMake is not installed."
+fi
+
+if (command -v ninja > /dev/null 2>&1); then
+	CMAKE_GENERATOR="Ninja"
+else
+	CMAKE_GENERATOR="Unix Makefiles"
+fi
+
+cmake -S . -B build -Dapply_optimization_options=ON -DCMAKE_BUILD_TYPE=Release -G $CMAKE_GENERATOR
+cd build
+cmake --build . --parallel
+./engine_sandbox
+cd ..
